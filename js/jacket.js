22f59cf55c55939604efa93b4bd1b7b2eceee185
Athen Lee <cupid.jack505@gmail.com>
2024-10-29 11:26:09 -0300

    in book_jacket.php, modal style modify

--- js/jacket.js
+++ js/jacket.js
@@ -142,153 +142,153 @@ function initModalEffects() {
                 success: function (response) { // If the request is successful and get response,
                     // then update modal body with response
 
-                    // let detailInfo = JSON.parse(response);
+                    let detailInfo = JSON.parse(response);
 
-                    // $("#thumb-alt-text").html("what is your name?");
-                    // $("#thumb-alt-img").attr("src", `assets/img/item/${defaultColor}.png`);
-                    // // $("#cover-img").attr("src", "assets/img/jackets/image1.jpeg");
-                    // $("#cover-img").attr("src", detailInfo.coverImgUrl);
-                    // $("#avail-img").attr("src", `assets/img/item/${detailInfo['status']}.png`);
-                    // $("#type-img").attr("src", `assets/img/item/${detailInfo['type']}.png`);
-                    // $("#title").html(detailInfo.title);
-                    // $("#bookTitle").html(detailInfo.title);
-                    // $(".thumb-alt-text").html(detailInfo.title);
+                    $("#thumb-alt-text").html("what is your name?");
+                    $("#thumb-alt-img").attr("src", `assets/img/item/${defaultColor}.png`);
+                    // $("#cover-img").attr("src", "assets/img/jackets/image1.jpeg");
+                    $("#cover-img").attr("src", detailInfo.coverImgUrl);
+                    $("#avail-img").attr("src", `assets/img/item/${detailInfo['status']}.png`);
+                    $("#type-img").attr("src", `assets/img/item/${detailInfo['type']}.png`);
+                    $("#title").html(detailInfo.title);
+                    $("#bookTitle").html(detailInfo.title);
+                    $(".thumb-alt-text").html(detailInfo.title);
 
-                    // // This is to make Copies part in Modal
-                    // let copiesHTML = "";
+                    // This is to make Copies part in Modal
+                    let copiesHTML = "";
 
-                    // detailInfo.copies.forEach(copyItem => {
-                    //     copiesHTML += `
-                    //         <p class="text-gray subtitle">Shelf Location at ${copyItem.position}</p>
-                    //         <table class="table table-bordered">
-                    //             <thead class="t-head">
-                    //                 <tr>
-                    //                     <th scope="col">Collection</th>
-                    //                     <th scope="col">Call Number</th>
-                    //                     <th scope="col">Status</th>
-                    //                 </tr>
-                    //             </thead>
-                    //             <tbody>
-                    //         `;
+                    detailInfo.copies.forEach(copyItem => {
+                        copiesHTML += `
+                            <p class="text-gray subtitle">Shelf Location at ${copyItem.position}</p>
+                            <table class="table table-bordered">
+                                <thead class="t-head">
+                                    <tr>
+                                        <th scope="col">Collection</th>
+                                        <th scope="col">Call Number</th>
+                                        <th scope="col">Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                            `;
                         
-                    //     copyItem.collections.forEach(collectionItem => {
-                    //         copiesHTML += `
-                    //                 <tr>
-                    //                     <td>${collectionItem[0]}</td>
-                    //                     <td>${collectionItem[1]}</td>
-                    //                     <td class="fw-bold">${collectionItem[2]}</td>
-                    //                 </tr>
-                    //         `;
-                    //     });
+                        copyItem.collections.forEach(collectionItem => {
+                            copiesHTML += `
+                                    <tr>
+                                        <td>${collectionItem[0]}</td>
+                                        <td>${collectionItem[1]}</td>
+                                        <td class="fw-bold">${collectionItem[2]}</td>
+                                    </tr>
+                            `;
+                        });
 
-                    //     copiesHTML += `
-                    //             </tbody>
-                    //         </table>
-                    //     `;
-                    // });
+                        copiesHTML += `
+                                </tbody>
+                            </table>
+                        `;
+                    });
 
-                    // // This is to make Summary part in Modal
-                    // let summaryHTML = "";
+                    // This is to make Summary part in Modal
+                    let summaryHTML = "";
 
-                    // summaryHTML += `
-                    //     <div class="bg-blue-heading kalam">Summary</div>
-                    // `;
+                    summaryHTML += `
+                        <div class="bg-blue-heading kalam">Summary</div>
+                    `;
 
-                    // detailInfo.summary.forEach(summaryItem => {
-                    //     summaryHTML += `
-                    //         <p class="summary-content">${summaryItem}</p>
-                    //     `;
-                    // });
+                    detailInfo.summary.forEach(summaryItem => {
+                        summaryHTML += `
+                            <p class="summary-content">${summaryItem}</p>
+                        `;
+                    });
 
-                    // // This is to make Reading Levels part in Modal
-                    // let levelsHTML = "";
+                    // This is to make Reading Levels part in Modal
+                    let levelsHTML = "";
 
-                    // let levelStrList = [
-                    //     "Lexile Measure",
-                    //     "AR Reading Level",
-                    //     "AR Interest Level",
-                    //     "AR Points"
-                    // ]
+                    let levelStrList = [
+                        "Lexile Measure",
+                        "AR Reading Level",
+                        "AR Interest Level",
+                        "AR Points"
+                    ]
 
-                    // detailInfo.levels.forEach((levelItem, index) => {
-                    //     if (levelItem) {
-                    //         levelsHTML += `
-                    //             <div class="d-flex flex-column">
-                    //                 <div class="fw-bold">${levelStrList[index]}</div>
-                    //                 <span class="neg-margin">${levelItem}</span>
-                    //             </div>
-                    //         `;
-                    //     }
-                    // })
+                    detailInfo.levels.forEach((levelItem, index) => {
+                        if (levelItem) {
+                            levelsHTML += `
+                                <div class="d-flex flex-column">
+                                    <div class="fw-bold">${levelStrList[index]}</div>
+                                    <span class="neg-margin">${levelItem}</span>
+                                </div>
+                            `;
+                        }
+                    })
 
-                    // // This is to make Details part in Modal
-                    // let detailsHTML = "";
+                    // This is to make Details part in Modal
+                    let detailsHTML = "";
 
-                    // if (detailInfo.details.author.length) { // If there is author list in detail information
-                    //     detailsHTML += `
-                    //         <div class="d-flex flex-column my-1">
-                    //             <div class="fw-bold">It's by</div>
-                    //     `;
+                    if (detailInfo.details.author.length) { // If there is author list in detail information
+                        detailsHTML += `
+                            <div class="d-flex flex-column my-1">
+                                <div class="fw-bold">It's by</div>
+                        `;
 
-                    //     detailInfo.details.author.forEach(author => {
-                    //         detailsHTML += `
-                    //             <a href="book_jacket.php?filterTerm=author&author=${author}" class="text-decoration-none neg-margin">${author}, author.</a>
-                    //         `;
-                    //     })
+                        detailInfo.details.author.forEach(author => {
+                            detailsHTML += `
+                                <a href="book_jacket.php?filterTerm=author&author=${author}" class="text-decoration-none neg-margin">${author}, author.</a>
+                            `;
+                        })
 
-                    //     detailsHTML += `
-                    //         </div>
-                    //     `;
-                    // }
+                        detailsHTML += `
+                            </div>
+                        `;
+                    }
 
-                    // if (detailInfo.details.belongs.length) { // If there is belongs list in detail information
-                    //     detailsHTML += `
-                    //         <div class="d-flex flex-column my-1">
-                    //             <div class="fw-bold">It's part of the series</div>
-                    //     `;
+                    if (detailInfo.details.belongs.length) { // If there is belongs list in detail information
+                        detailsHTML += `
+                            <div class="d-flex flex-column my-1">
+                                <div class="fw-bold">It's part of the series</div>
+                        `;
 
-                    //     detailInfo.details.belongs.forEach(belong => {
-                    //         detailsHTML += `
-                    //             <a href="book_jacket.php?filterTerm=seriesTitle&seriesTitle=${belong}" class="text-decoration-none neg-margin">${belong}</a>
-                    //         `;
-                    //     });
+                        detailInfo.details.belongs.forEach(belong => {
+                            detailsHTML += `
+                                <a href="book_jacket.php?filterTerm=seriesTitle&seriesTitle=${belong}" class="text-decoration-none neg-margin">${belong}</a>
+                            `;
+                        });
                         
-                    //     detailsHTML += `
-                    //         </div>
-                    //     `;
-                    // }
+                        detailsHTML += `
+                            </div>
+                        `;
+                    }
 
-                    // if (detailInfo.details.length) { // If there is belongs list in detail information
-                    //     detailsHTML += `
-                    //         <div class="d-flex flex-column mt-2">
-                    //             <div class="fw-bold">Length</div>
-                    //             <span class="neg-margin">${detailInfo.details.length}</span>
-                    //         </div>
-                    //     `;
-                    // }
+                    if (detailInfo.details.length) { // If there is belongs list in detail information
+                        detailsHTML += `
+                            <div class="d-flex flex-column mt-2">
+                                <div class="fw-bold">Length</div>
+                                <span class="neg-margin">${detailInfo.details.length}</span>
+                            </div>
+                        `;
+                    }
 
-                    // if (detailInfo.details.relates.length) { // If there is author list in detail information
-                    //     detailsHTML += `
-                    //         <div class="d-flex flex-column my-1">
-                    //             <div class="fw-bold">Related Things</div>
-                    //     `;
+                    if (detailInfo.details.relates.length) { // If there is author list in detail information
+                        detailsHTML += `
+                            <div class="d-flex flex-column my-1">
+                                <div class="fw-bold">Related Things</div>
+                        `;
 
-                    //     detailInfo.details.relates.forEach(relatedItem => {
-                    //         detailsHTML += `
-                    //             <a href="book_jacket.php?filterTerm=searchBox&searchBox=${relatedItem}" class="text-decoration-none neg-margin">${relatedItem}</a>
-                    //         `;
-                    //     })
+                        detailInfo.details.relates.forEach(relatedItem => {
+                            detailsHTML += `
+                                <a href="book_jacket.php?filterTerm=searchBox&searchBox=${relatedItem}" class="text-decoration-none neg-margin">${relatedItem}</a>
+                            `;
+                        })
 
-                    //     detailsHTML += `
-                    //         </div>
-                    //     `;
-                    // }
+                        detailsHTML += `
+                            </div>
+                        `;
+                    }
 
-                    // // Then update the Modal content using above values
-                    // $("#copies")   .html(copiesHTML);
-                    // $("#summary")  .html(summaryHTML);
-                    // $("#levels")   .html(levelsHTML);
-                    // $("#details")  .html(detailsHTML);
+                    // Then update the Modal content using above values
+                    $("#copies")   .html(copiesHTML);
+                    $("#summary")  .html(summaryHTML);
+                    $("#levels")   .html(levelsHTML);
+                    $("#details")  .html(detailsHTML);
 
                     $("#modal").css("display", "flex"); // After then, Show the modal
                 }
@@ -422,31 +422,38 @@ function initModalEffects() {
         var remember = $('#remember').is(':checked');
 
         if (!name) {
+            $(".dogSaying").css("display", "block");
+            $(".dogSaying .suggest").css("display", "block");
             $(".say-content.suggest").html("Please input the Name.");
-        }
-
-        if (!email) {
+            $(".dogImg").css("display", "block");
+            $(".dogImg").css("z-index", "100000");
+            resetDogSaying();
+        } else if (!email) {
+            $(".dogSaying").css("display", "block");
+            $(".dogSaying .suggest").css("display", "block");
             $(".say-content.suggest").html("Please input the Email.");
+            $(".dogImg").css("display", "block");
+            $(".dogImg").css("z-index", "100000");
+            resetDogSaying();
+        } else {
+            $("#name").val("");
+            $("#email").val("");
+    
+            $.ajax({
+                url: 'mail_handler.php', // URL to Mail.php
+                type: 'POST', // If then, you can get the request value(jacket/boot ID) using $_POST['id]
+                data: { name: name, email: email, remember, record_id }, // This is the request value
+                success: function () {
+                    console.log("successfully sent");
+                    $("#modal").css("display", "none");
+                    hideElement(".login");
+                    hideElement(".dogSaying");
+                    $(".dogImg").css("z-index", "10");
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+            })
         }
-
-        $("#name").val("");
-        $("#email").val("");
-
-        $.ajax({
-            url: 'mail_handler.php', // URL to Mail.php
-            type: 'POST', // If then, you can get the request value(jacket/boot ID) using $_POST['id]
-            data: { name: name, email: email, remember, record_id }, // This is the request value
-            success: function () {
-                console.log("successfully sent");
-                $("#modal").css("display", "none");
-                hideElement(".login");
-                hideElement(".dogSaying");
-                $(".dogImg").css("z-index", "10");
-            },
-            error: function (err) {
-                console.log(err);
-            }
-        })
-
     })
 }
